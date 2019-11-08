cf9aa3d358d76609942a97806d1a9f66a949292a
zefurr <onetimeuseaddress@gmail.com>
2019-11-08 13:00:22 -0500

    Server side Registration done Client side TBD

--- UDPClient/UDPClient.cpp
+++ UDPClient/UDPClient.cpp
@@ -4,8 +4,8 @@
 
 #include<stdio.h>
 #include<winsock2.h>
-#pragma comment(lib,"ws2_32.lib")	//Winsock Library
-#define SERVER "127.0.0.1"			//ip address of udp server
+#pragma comment(lib,"ws2_32.lib")	// Winsock Library
+#define SERVER "127.0.0.1"			// ip address of udp server
 #define BUFLEN 512					// Max length of buffer
 #define PORT 8888					// The port on which to listen for incoming data
 int main(void)
