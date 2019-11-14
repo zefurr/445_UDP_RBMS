b9ed0b9809ad93d9fc7e4b817f6cd479b98a9805
zefurr <onetimeuseaddress@gmail.com>
2019-11-14 17:06:27 -0500

    Prepare for architecture redesign

---
+++ UDPServer/Message.h
@@ -0,0 +1,61 @@
+//#include <winsock2.h>
+//#include <stdio.h>
+//#include <string>
+//#include <time.h>
+//#include <iomanip>
+
+//#include <iostream>
+//#include <chrono>
+//#include <ctime>
+//#include <time.h>
+//#include <sstream>
+
+#pragma once
+
+#include "pch.h"
+#include <vector>
+
+
+// definitions for messages received from clients TEST
+#define MSG_REG "REGISTERbutITisREALLYlongINSTEADdoesTHISchangeANYTHING"
+
+// definitions for messaged send by the server
+#define MSG_START "START"
+#define ACK_REG "ACK_REGISTER"
+
+// Messages which carry no data (Registration, Acknowledgements)
+struct BaseMessage
+{
+
+	std::string msgType;
+
+	BaseMessage();
+	BaseMessage(std::string);
+};
+
+// Message sent from server to each client to inform them of other participants
+struct SessionStartMsg : public BaseMessage
+{
+	std::vector<std::string> participants;
+	SessionStartMsg(std::string, std::vector<std::string>);
+};
+
+// Message sent from client to server to request a meeting booking
+struct RequestMsg : public BaseMessage
+{
+};
+
+struct InviteMsg : public BaseMessage
+{
+
+};
+
+struct AcceptMsg : public BaseMessage
+{
+
+};
+
+struct RejectMsg : public BaseMessage
+{
+
+};
